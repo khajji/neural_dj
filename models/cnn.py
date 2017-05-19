39e7b2e70d8a9230712588f9f672692abc553393
Khalil Hajji <khalilhajji@dhcp-rhodes-440.eduroam.cornell.edu>
2017-05-18 23:20:58 -0400

    cnn code

--- models/cnn.py
+++ models/cnn.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python
 import os, sys, pdb
 import numpy as np
-sys.path.insert(0, os.path.abspath("../data_processing/"))
+sys.path.insert(0, os.path.abspath("../dataio/"))
 from dataio import *
 import matplotlib.pyplot as plt
 import tensorflow as tf
 class Cnn:
-	def __init__(x_shape, y_shape, netwrok_specs): 
-		#x_shape = [batch, height, width, channels] 
+	def __init__(self, data_specs, network_specs): 
+		#data_specs = (x_shape, y_shape, xnetwork_shape, ynetwork_shape)
 		#y_shape=[batch, height, width, channels]
 		#network_specs = #[(['conv', ('height', 'width', 'depth'), 'activation'], number), (['connected', (outsize), 'activation'], number)]
-		self.specs = netwrok_specs; self.x_shape = x_shape; self.y_shape = y_shape
-		self.loss = None
-		self.y_hat=None
+		self.specs = network_specs; 
+		self.x_shape, self.y_shape, self.xcnn_shape, self.ycnn_shape = data_specs
+		#tensors
+		self.x, self.y, self.xnn, self.ycnn = None, None, None, None
+		self.ycnn_hat, self.y_hat=None, None
+		self.loss = None;
+		self.dropout = None 
+		self.build_network()
 
 
-	def build_network():
+	def build_network(self):
 		
-		xshape, yshape, specs = specs[0], specs[1], specs[2:]
-		x = tf.placeholder(tf.float32, shape=self.x_shape)
-		y = tf.placeholder(tf.float32, shape=self.y_shape)
-		z=x
+		specs = self.specs
+		self.x = tf.placeholder(tf.float32, shape=self.x_shape)
+		self.y = tf.placeholder(tf.float32, shape=self.y_shape)
+		self.xcnn = tf.reshape(self.x, Cnn.reshape(self.xcnn_shape))#[-1,1,self.x_shape,1]
+		self.ycnn = tf.reshape(self.y, Cnn.reshape(self.ycnn_shape)) #[-1,1,self.y_shape,1]
+		z=self.xcnn; self.dropout = tf.placeholder(tf.float32) #dropout probability holder
+		
+		
 		for spec in self.specs:
 			([layer, params, activation], depth) = spec
-			for in i in range(depth):
+			for i in range(depth):
 				if layer == 'conv':
 					(height, width, depth)=params
-					z = convolution_layer(z, height, width, depth , activation)
+					z = Cnn.convolution_layer(z, height, width, depth , activation)
 				elif layer == 'connected':
 					(outsize) = params
-					z =  connected_layer(z, outsize , activation)
+					z =  Cnn.connected_layer(z, outsize , activation)
+				elif layer == 'dropout':
+					z = Cnn.dropout_layer(z, self.dropout)
 
-		self.y_hat=z
-		self.loss = tf.reduce_mean(tf.square(tf.subtract(y_hat, y)))
+		self.ycnn_hat=tf.reshape(z, Cnn.reshape(self.ycnn_shape))
+		self.loss = tf.reduce_mean(tf.square(tf.subtract(self.ycnn_hat, self.ycnn)))
 
-	def train(xTr, yTr, batchsize=50, n_training=10000):
+		self.y_hat=tf.reshape(self.ycnn_hat, Cnn.reshape(self.y_shape))
+
+
+	def train(self, xTr, yTr, dropout=0.5, batchsize=50, n_training=10000): #data are file paths
 		#Xtr list of files, Ytr list of files
 		train_step = tf.train.AdamOptimizer(1e-4).minimize(self.loss) #we use adam method to minimize the loss
 
@@ -42,48 +56,72 @@ class Cnn:
 		sess.run(tf.global_variables_initializer())
 
 		for i in range(n_training):
-			x_batch, y_batch = sample_batch(xTr, ytr, batchsize, i)
-			train_step.run(feed_dict={x: x_batch, y_in: y_batch})
+			x_batch, y_batch = sample_batch(xTr, yTr, batchsize, i)
 
+			if i%10==0:
+				#pdb.set_trace()
+				loss = self.loss.eval(feed_dict={self.x: x_batch, self.y: y_batch, self.dropout: dropout})
+				print ("iteration "+str(i)+": loss = "+str(loss))
+
+			train_step.run(feed_dict={self.x: x_batch, self.y: y_batch, self.dropout: dropout})
+
+		'''
+		xVl, yVl = loadx(xVl), loady(yVl)
+		y_pred=self.y_hat.eval(feed_dict={self.x: xVl, self.dropout:0.0})
+		score = self.loss.eval(feed_dict={self.x: xVl, self.y: yVl, self.dropout:0.0})
+		return y_pred, score'''
 		sess.close()
+		
 
 		
 
-	def test(xVl, yVl):
+	def test(self, xVl, yVl): #data are file paths
+		xVl, yVl = loadx(xVl), loady(yVl)
 		sess = tf.InteractiveSession()
-		sess.run(tf.global_variables_initializer())
-		y_pred=self.y_hat.eval(feed_dict={x: xVl})
-		score = self.loss.eval()accuracy.eval(feed_dict={x: xVe, y_: yVl})
 
-		return y_pred, loss
+		sess.run(tf.global_variables_initializer())
+		y_pred=self.y_hat.eval(feed_dict={self.x: xVl, self.dropout:0})
+		score = self.loss.eval(feed_dict={self.x: xVl, self.y: yVl, self.dropout:0})
+		sess.close()
+		return y_pred, score
 
 	@staticmethod
 	def weight_variable(shape):
 		initial = tf.truncated_normal(shape, stddev=0.1)
 		return tf.Variable(initial)
-	@staticmethod
-	def conv2d(x, W):
-		return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+	@staticmethod	
+	def bias_variable(shape):
+  		initial = tf.constant(0.1, shape=shape)
+  		return tf.Variable(initial)
 
 	@staticmethod
-	def convolution_layer(x, height, width, depth , activation):
-		height, width, in_channels, depth = filter_height, filter_width, x.shape()[3], out_channels
-		w = weight_variable([height, width, in_channels, depth])
-		b = bias_variable([depth])
-		conv = conv1d(z, w, padding='SAME'
-    	stride=[1,1,1,1], #stride on [filter_height, filter_width, in_channels, out_channels]
-    	)
+	def convolution_layer(x, filter_height, filter_width, filter_depth , activation):
+		height, width, in_channels, depth = filter_height, filter_width, x.get_shape().as_list()[3], filter_depth
+		w = Cnn.weight_variable([height, width, in_channels, depth])
+		b = Cnn.bias_variable([depth])
+		conv = tf.nn.conv2d(x, w, padding='SAME', strides=[1,1,1,1]) #stride on [filter_height, filter_width, in_channels, out_channels]
 		z = activation(conv + b)
 		return z
 
 	@staticmethod
 	def connected_layer(x, output_size , activation):
-		[batch, in_height, in_width, in_channels]=x.shape()
-		insize, outsize = in_height*in_width*in_channels, outsize
-		w = weight_variable([insize, outsize])
-		b = bias_variable([outsize])
+		[batch, in_height, in_width, in_channels]=x.get_shape().as_list()
+		insize, outsize = in_height*in_width*in_channels, output_size
+		x = tf.reshape(x, [-1, in_height*in_width*in_channels])
+		w = Cnn.weight_variable([insize, outsize])
+		b = Cnn.bias_variable([outsize])
 		z = activation(tf.matmul(x, w) + b)
 		return z
+
+	@staticmethod
+	def dropout_layer(x, dropout_tensor):
+		z = tf.nn.dropout(x, 1-dropout_tensor) 
+		return z
+
+	@staticmethod
+	def reshape(shape):
+		return [s if s is not None else -1 for s in shape]
 
 
 
---
+++ models/golearner.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import os, sys, pdb
+import numpy as np
+sys.path.insert(0, os.path.abspath("../dataio/"))
+from dataio import *
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from cnn import *
+
+data_path="../data_sample/pprocessed_data2"
+prediction_path = "../data_sample/predictions"
+#pdb.set_trace()
+dataset = Dataset(data_path)
+xTr, yTr, xVl, yVl = dataset.split(ratio=0.8)
+dx = 784
+dy = 784
+#x_shape = [batch, height, width, channels] 
+#y_shape=[batch, height, width, channels]
+#network_specs = #[(['conv', ('height', 'width', 'depth'), 'activation'], number), (['connected', (outsize), 'activation'], number)]
+network_specs = [
+				 (['conv',(5, 5, 32), tf.nn.relu], 1), #layer 1: convolution layer with a filter of 1*25 and a depth of 32 using relu as an activation function
+				 (['conv', (5,5,64), tf.nn.relu], 1), #layer 2: convolution layer with a filter of 1*25 and a depth of 64 using relu as an activation function
+				 (['connected', (dy), tf.nn.relu], 1), #layer 3: fully connected layer with output size dy and relu activation function
+				 (['dropout',   None, None],     1)]
+##data_specs = (x_shape, y_shape, xnetwork_shape, ynetwork_shape)
+data_specs = ([None, dx], [None, dy], [None,28,28,1], [None,28,28,1])
+cnn = Cnn(data_specs, network_specs)
+
+#cnn.build_network(xTr, yTr, batchsize=50, n_training=1)
+cnn.train(xTr, yTr, batchsize=50, n_training=200)
+
+y_pred, loss = cnn.test(xVl, yVl)
+
+print("loss on validation set : "+str(loss))
+
+output_names = dataset.give_outputs()
+save(y_pred, prediction_path, output_names)
+
+
+
+'''
+network_specs = [
+				 (['conv',(1, 25, 32), tf.nn.relu], 1), #layer 1: convolution layer with a filter of 1*25 and a depth of 32 using relu as an activation function
+				 (['conv', (1,25,64), tf.nn.relu], 1), #layer 2: convolution layer with a filter of 1*25 and a depth of 64 using relu as an activation function
+				 (['connected', (dy), tf.nn.relu], 1), #layer 3: fully connected layer with output size dy and relu activation function
+				 (['dropout',   None, None],     1)]
+
+data_specs = ([None, dx], [None, dy], [None,1,dx,1], [None,1,dy,1])
+'''
